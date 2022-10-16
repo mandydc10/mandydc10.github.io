2e4c4efe05f0e379c7aa9bec38f4a7170236797f
mandydc10 <85203962+mandydc10@users.noreply.github.com>
2022-10-16 13:12:59 +0800

    Change of theme and begun styling for mobile responsive
    Overhaul of theme from purple to grey
    Started responsive styling, mostly for mobile with a dropdown menu

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mandydc10.github.io
  
+Website link: 
+mandydc10.github.io
+
+Repository link:
+https://github.com/mandydc10/mandydc10.github.io
---
+++ apps.js
@@ -0,0 +1,10 @@
+/* Set the width of the side navigation to 250px */
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primary-navigation');
+
+navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible") 
+  ? navToggle.setAttribute("aria-expanded", false)
+  : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+})
